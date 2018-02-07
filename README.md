7a5bd2d816eb71f0608f4306269b970f6ee05d5d
Chase McCoy <chasem000@gmail.com>
2018-02-07 14:11:43 -0600

    Add nowPlaying and productivity endpoints

--- README.md
+++ README.md
@@ -2,22 +2,21 @@
 
 A tiny Node.js microservice to grab some stats from around the web.
 
+
 ## Deployment
 
 Deploying `micro-stats` is one click away:
 
 [![Deploy to now](https://deploy.now.sh/static/button.svg)](https://deploy.now.sh/?repo=https://github.com/chasemccoy/micro-stats)
 
-You can also deploy with a single command from the terminal (assuming you have [`now`](https://now.sh) installed):
-
-```sh
-now chasemccoy/micro-stats
-```
 
 ## Usage
 
 **Available Endpoints:**
 * `/?type=weather`: Returns the current conditions for Chicago, IL.
+* `/?type=productivity`: Returns the productivity pulse score from RescueTime.
+* `/?type=nowPlaying`: Returns an object representing the current song playing from Spotify in the format `{name, artist, album}`.
+
 
 ## Development
 
--- index.js
+++ index.js
@@ -1,23 +1,53 @@
 const { parse } = require('url')
+const { send } = require('micro')
 const got = require('got');
 
-const hello = () => 'Hello world!'
+const getJSONResponse = async (url) => {
+	const { body: response } = await got(url, {json: true});
+	return response
+}
+
+const error = (res) => {
+	send(res, 400, 'Plase specify a type of data to retrieve.')
+}
 
 const weather = async () => {
-	const { body: response } = await got('https://api.darksky.net/forecast/47b4c46a2eba602b069144b4b6310e09/41.8781,87.6298', {json: true});
+	const data = await getJSONResponse('https://api.darksky.net/forecast/47b4c46a2eba602b069144b4b6310e09/41.8781,87.6298')
 
-	return response.currently.summary
+	return data.currently.summary
+}
+
+const productivity = async () => {
+	const data = await getJSONResponse('https://www.rescuetime.com/anapi/daily_summary_feed?key=B63oGksutbJmDQDM6LkXh5b074UJTJEkprmLqdyX')
+
+	return data[0].productivity_pulse
+}
+
+const nowPlaying = async () => {
+	const data = await getJSONResponse('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=chase_mccoy&api_key=419ac16cc2e1ad92a9520006a5d7c575&format=json')
+
+	const track = data.recenttracks.track[0]
+
+	return {
+		name: track.name,
+		artist: track.artist['#text'],
+		album: track.album['#text']
+	}
 }
 
 module.exports = async (req, res) => {
   const { query: { type } } = parse(req.url, true)
 
-  if (!type) return hello()
+  if (!type) return error(res)
 
 	switch (type) {
 		case 'weather':
 			return weather()
+		case 'productivity':
+			return productivity()
+		case 'nowPlaying':
+			return nowPlaying()
 		default:
-			return hello()
+			return error(res)
 	}
 }
